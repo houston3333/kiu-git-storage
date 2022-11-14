30e170d257626d69ad5b29993b0b042b8d1a4179
houston13 <57140304+gulfiya33@users.noreply.github.com>
2022-11-14 18:49:05 +0300

    Update hm1.py

--- hm1.py
+++ hm1.py
@@ -1,4 +1,3 @@
 def solution(string):
-    if len(string) % 2 != 0: 
-        string += '_'
+    if (len(string) % 2 != 0): string += '_'
     return [string[i:i+2] for i in range(0, len(string), 2)]
