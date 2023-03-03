ed8f2dbecf088a435e3b5bc245c75769be529f38
houston13 <57140304+houston3333@users.noreply.github.com>
2023-03-03 18:33:50 +0300

    Update f.py

--- f.py
+++ f.py
@@ -1,62 +1,51 @@
 import abc
 
 
-class GUIFactory(metaclass=abc.ABCMeta):
+class AbstractFactory(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
-    def createButton(self,color):
+    def create_button(self):
         pass
-    
+
     @abc.abstractmethod
-    def createCheckbox(self,color):
+    def create_checkbox(self):
         pass
 
 
+class MacFactory(AbstractFactory):
 
-class WinFactory(GUIFactory):
+    color = 'white'
 
-    def create_product_a(self):
-        return createButton()
+    def create_checkbox(self):
+        return Checkbox()
 
-    def create_product_b(self):
-        return createCheckbox()
+    def create_button(self):
+        return TouchButton()
 
 
-class MacFactory(GUIFactory):
-    
-    def create_product_a(self):
-        return createButton()
+class WindowsFactory(AbstractFactory):
 
-    def create_product_b(self):
-        return createCheckbox()
+    color = 'blue'
+
+    def create_checkbox(self):
+        return Checkbox()
+
+    def create_button(self):
+        return TouchButton()
 
 
 class AbstractButton(metaclass=abc.ABCMeta):
-    def __init__(self, color):
-        self.color = color
     
+    def init(self, color):
+        self.color = color
 
-class createButton(AbstractButton):
-   
-    def TouchButton(self):
+class TouchButton(AbstractButton):
+
+    def touch(self):
         print('Кнопка нажата')
 
 
-class createCheckbox(AbstractButton):
+class Checkbox(AbstractButton):
 
     def check(self):
         print('Галочка поставлена')
-
-
-
-def main():
-    for factory in (WinFactory(), MacFactory()):
-        product_a = factory.create_product_a() 
-        product_b = factory.create_product_b()
-
-        product_a.TouchButton()
-        product_b.check()
-
-
-if __name__ == "__main__":
-    main()
